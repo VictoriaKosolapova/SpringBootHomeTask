ff3e4652b40b8ab6383c1a88c019e2aca3bdad19
Viktor Sukharev <visu0215@netcracker.com>
2018-12-13 18:57:29 +0300

    add post example

--- src/main/java/com/netcracker/edu/db/employee/web/EmployeeController.java
+++ src/main/java/com/netcracker/edu/db/employee/web/EmployeeController.java
@@ -3,8 +3,12 @@ package com.netcracker.edu.db.employee.web;
 import com.netcracker.edu.db.employee.model.Employee;
 import com.netcracker.edu.db.employee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +22,14 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
     @GetMapping("/{id}")
-    public Employee getEmployeeById(@PathVariable("id") BigInteger employeeId) {
-        return employeeService.getEmployeeById(employeeId);
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") BigInteger employeeId) {
+        return ResponseEntity.ok(employeeService.getEmployeeById(employeeId));
+    }
+
+    @PostMapping(path = "/add", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity addEmployee(@RequestBody Employee employee) {
+        System.out.println(employee);
+
+        return ResponseEntity.ok().build();
     }
 }
